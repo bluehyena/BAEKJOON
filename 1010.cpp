84bd04becb328003e637c1a0c0180e425008aec9
bluehyena <42088479+bluehyena@users.noreply.github.com>
2021-02-05 07:24:52 +0900

    [Add] - Add 1010

---
+++ 1010.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+
+	unsigned long long T, N, M;
+	cin >> T;
+
+	for (int i = 0; i < T; ++i) {
+		unsigned long long bridge = 1;
+		unsigned long long small, large;
+
+		cin >> N >> M;
+
+		if (M - N > N) {
+			large = M - N;
+			small = N;
+		}
+		else {
+			large = N;
+			small = M - N;
+		}
+
+		if (M != N) {
+			for (unsigned long long j = M; j > large; --j) {
+				bridge *= j;
+			}
+			for (unsigned long long k = 2; k <= small; ++k) {
+				bridge /= k;
+			}
+		}
+		cout << bridge << endl;
+	}
+	return 0;
+}
