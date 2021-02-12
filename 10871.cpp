877c5c66b7bac0fa75bcad35c4e01ea471d2ed24
bluehyena <42088479+bluehyena@users.noreply.github.com>
2021-02-13 06:04:19 +0900

    [Add] - Add 108710

---
+++ 10871.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+int arr[10001];
+
+int main() {
+
+	int N, X;
+	cin >> N >> X;
+
+	for (int i = 1; i <= N; ++i) {
+		cin >> arr[i];
+	}
+	
+	for (int i = 1; i <= N; ++i) {
+		if (X > arr[i]) {
+			cout << arr[i] << ' ';
+		}
+	}
+
+	return 0;
+}
