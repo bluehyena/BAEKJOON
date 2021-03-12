1a91a3176fd1dd50cb920823b80f89024ccfe86f
bluehyena <42088479+bluehyena@users.noreply.github.com>
2021-03-13 03:26:52 +0900

    [FIX] revise number of problem

--- 1874.cpp
+++ 1874.cpp
@@ -1,64 +1,50 @@
 #include <iostream>
 #include <stack>
+#include <vector>
 
 using namespace std;
 
-int main() {
+int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	string N;
+	int N, number;
+	stack<int> stk;
+	vector<int> num;
+	vector<char> vec;
+
 	cin >> N;
 
-	stack<char> stk;
-	int answer = 0;
-	int tmp = 1;
+	for (int i = 0; i < N; ++i) {
+		cin >> number;
+		num.push_back(number);
+	}
 
-	for (int i = 0; i < N.length(); ++i) {
-		if (N[i] == '(') {
-			tmp *= 2;
-			stk.push(N[i]);
-		}
-		else if (N[i] == '[') {
-			tmp *= 3;
-			stk.push(N[i]);
-		}
-		else if (N[i] == ')') {
-			if (stk.empty()) {
-				stk.push(N[i]);
-			}
-			else if (N[i - 1] == '(') {
+	int idx = 0;
+	for (int i = 1; i <= N; ++i) {
+		stk.push(i);
+		vec.push_back('+');
+
+		while (!stk.empty()) {
+			if (num[idx] == stk.top()) {
+				vec.push_back('-');
 				stk.pop();
-				answer += tmp;
-				tmp /= 2;
+				++idx;
 			}
-			else if (stk.top() == '(') {
-				stk.pop();
-				tmp /= 2;
-			}
-		}
-		else if (N[i] == ']') {
-			if (stk.empty()) {
-				stk.push(N[i]);
-			}
-			else if (N[i - 1] == '[') {
-				stk.pop();
-				answer += tmp;
-				tmp /= 3;
-			}
-			else if (stk.top() == '[') {
-				stk.pop();
-				tmp /= 3;
+			else {
+				break;
 			}
 		}
 	}
 
 	if (stk.empty()) {
-		cout << answer;
+		for (int i = 0; i < vec.size(); ++i) {
+			cout << vec[i] << "\n";
+		}
 	}
 	else {
-		cout << 0;
+		cout << "NO" << "\n";
 	}
 
 	return 0;
