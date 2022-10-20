44229981c2150366a10e6dcc4b0503dc58b87ba1
Jun Hyeok Lee <bluehyena123@naver.com>
2022-10-20 23:26:39 +0900

    [NEW] - create 1402.cpp

---
+++ 1402.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t;
+    int a, b;
+
+    cin >> t;
+
+    for (int i = 0; i < t; ++i){
+        cin >> a >> b;
+        cout << "yes" << endl;
+    }
+
+    return 0;
+}
