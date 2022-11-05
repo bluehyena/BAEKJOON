3073ab603f38661d92beda0bad3975911982c758
Jun Hyeok Lee <bluehyena123@naver.com>
2022-11-05 15:28:10 +0900

    [NEW] - create 11279.cpp

---
+++ 11279.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <queue>
+#include <functional> 
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int x;
+	cin >> x;
+
+	priority_queue<int> pq;
+
+	for (int i = 0; i < x; ++i) {
+		int a;
+		cin >> a;
+
+		if (a == 0) {
+			if (pq.empty()) {
+				cout << 0 << '\n';
+			}
+			else {
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+		}
+		else {
+			pq.push(a);
+		}
+	}
+
+	return 0;
+}
